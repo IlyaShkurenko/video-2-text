b33d19aa457bd4e1b43e786b5c0bfd6282af9511
illiashkurenko <ilya-shkurenko@mail.ru>
2024-10-22 19:23:57 +0300

    add openbmb

---
+++ openbmb.py
@@ -0,0 +1,35 @@
+import torch
+from PIL import Image
+from transformers import AutoModel, AutoTokenizer
+
+model = AutoModel.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True, torch_dtype=torch.float16)
+model = model.to(device='cuda')
+
+tokenizer = AutoTokenizer.from_pretrained('openbmb/MiniCPM-Llama3-V-2_5', trust_remote_code=True)
+model.eval()
+
+image = Image.open('https://storage.googleapis.com/tidy-federation-332618.appspot.com/img/frame_0001.jpg').convert('RGB')
+question = 'Detailed description of the image, each object and their location, and what they are doing. Location And Objects should be in coordinates, like monkey:(10, 30)'
+msgs = [{'role': 'user', 'content': question}]
+
+res = model.chat(
+    image=image,
+    msgs=msgs,
+    tokenizer=tokenizer,
+    sampling=True, # if sampling=False, beam_search will be used by default
+    temperature=0.7,
+    # system_prompt='' # pass system_prompt if needed
+)
+print(res)
+
+## if you want to use streaming, please make sure sampling=True and stream=True
+## the model.chat will return a generator
+res = model.chat(
+    image=image,
+    msgs=msgs,
+    tokenizer=tokenizer,
+    sampling=True,
+    temperature=0.7,
+    stream=False
+)
+print(res)
