fd7a0869ff3bfcd9791f553f145057efa91d8bb1
illiashkurenko <ilya-shkurenko@mail.ru>
2024-10-27 14:39:08 +0200

    refactoring + add xtts2

--- video-2-text/main.py
+++ video-2-text/main.py
@@ -17,37 +17,8 @@ model = LlavaNextVideoForConditionalGeneration.from_pretrained(
 ).to(device)
 
 processor = LlavaNextVideoProcessor.from_pretrained(model_id)
-# def download(url: str, dest_folder: str):
-#     if not os.path.exists(dest_folder):
-#         os.makedirs(dest_folder)  # create folder if it does not exist
-
-#     filename = url.split('/')[-1].replace(" ", "_")  # be careful with file names
-#     file_path = os.path.join(dest_folder, filename)
-
-#     r = requests.get(url, stream=True)
-#     if r.ok:
-#         print("saving to", os.path.abspath(file_path))
-#         with open(file_path, 'wb') as f:
-#             for chunk in r.iter_content(chunk_size=1024 * 8):
-#                 if chunk:
-#                     f.write(chunk)
-#                     f.flush()
-#                     os.fsync(f.fileno())
-#     else:  # HTTP status code 4XX/5XX
-#         print("Download failed: status code {}\n{}".format(r.status_code, r.text))
 
 def sample_frames(url, num_frames):
-    # path_id = str(uuid.uuid4())
-    # path = f"./{path_id}.mp4"
-    
-    # download(url, path)
-
-    # file_size = os.path.getsize(path)
-    # print(f"Video saved to {path}, size: {file_size} bytes")
-
-    # with open(path, "wb") as f:
-    #      f.write(response.content)
-
     video = cv2.VideoCapture(url)
     print(video)
     total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
---
+++ voice-cloning/xtts/main.py
@@ -0,0 +1,35 @@
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
+from IPython.display import Audio
+from scipy.io.wavfile import write
+import os
+
+config = XttsConfig()
+config.load_json("./XTTS-v2/config.json")
+
+model = Xtts.init_from_config(config)
+model.load_checkpoint(config, checkpoint_dir="./XTTS-v2/")
+model.cuda() 
+
+text_to_speak = "Technology is evolving faster than ever before, changing the way we live and work. Artificial intelligence is at the forefront of this transformation. It helps us solve complex problems, automate routine tasks, and create new opportunities. With tools like XTTS-v2, giving machines a voice has never been easier. Imagine a world where every device can speak in a natural and expressive way. The future of communication is already here!"
+
+reference_audios = [
+    os.path.join("..", "reference_audio1.wav"),
+    os.path.join("..", "reference_audio2.wav"),
+    os.path.join("..", "reference_audio3.wav")
+]
+
+outputs = model.synthesize(
+    text=text_to_speak,
+    config=config,
+    speaker_wav=reference_audios,
+    gpt_cond_len=5,
+    language="en"
+)
+
+Audio(data=outputs['wav'], rate=24000)
+
+output_file_path = './output.wav'
+write(output_file_path, 24000, outputs['wav'])
+
+print(f"output: {output_file_path}")
